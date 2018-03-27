7d9df4def3f229aecb0df041c1cca2fe0b8c5065
GitHubAlexKir <alexvanfontys@gmail.com>
2018-03-27 11:15:33 +0200

    Edit unit tests for registration before

--- src/test/java/com/devglan/controller/AuthenticationControllerTest.java
+++ src/test/java/com/devglan/controller/AuthenticationControllerTest.java
@@ -7,7 +7,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.devglan.model.LoginUser;
+import com.devglan.model.User;
+import com.devglan.model.UserDto;
+import com.devglan.service.UserService;
 import com.google.gson.Gson;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,16 +29,21 @@ public class AuthenticationControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    private String token;
-    private void setToken()
-    {
-
+    @Autowired
+    private UserService userService;
+    private User userDto;
+    @Before
+    public void register(){
+        UserDto userDto = new UserDto();
+        userDto.setUsername("UnitTestUser123456789");
+        userDto.setPassword("UnitTestPassword");
+        this.userDto = userService.save(userDto);
     }
     @Test
     public void AuthenticationIsOk() throws Exception {
         LoginUser user = new LoginUser();
-        user.setUsername("Alex");
-        user.setPassword("Alex");
+        user.setUsername(this.userDto.getUsername());
+        user.setPassword("UnitTestPassword");
         Gson gson = new Gson();
         String json = gson.toJson(user);
 
@@ -45,7 +55,7 @@ public class AuthenticationControllerTest {
     @Test
     public void AuthenticationIsFalse() throws Exception {
         LoginUser user = new LoginUser();
-        user.setUsername("root");
+        user.setUsername(this.userDto.getUsername());
         user.setPassword("wrongPassword");
         Gson gson = new Gson();
         String json = gson.toJson(user);
@@ -53,6 +63,11 @@ public class AuthenticationControllerTest {
         this.mockMvc.perform(post("/token/generate-token")
                 .contentType(MediaType.APPLICATION_JSON).content(json))
                 .andExpect(status().is(401));
+    }
+    @After
+    public void deleteUser()
+    {
+        this.userService.delete(this.userDto.getId());
     }
 
 }
